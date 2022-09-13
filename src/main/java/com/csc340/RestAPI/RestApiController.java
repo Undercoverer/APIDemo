34a2d7eb98e90f8fe303a73390404c6e1069fdc3
foxkj <fkjohnju@uncg.edu>
2022-09-13 16:29:22 -0400

    stuff things

--- src/main/java/com/csc340/RestAPI/RestApiController.java
+++ src/main/java/com/csc340/RestAPI/RestApiController.java
@@ -1,79 +1,69 @@
 package com.csc340.RestAPI;
 
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
 import org.json.JSONArray;
 import org.json.JSONObject;
-
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-/**
- *
- * @author sentini
- */
 @RestController
 public class RestApiController {
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello, %s!", name);
-    }
-
     /**
-     * Get a random quote from quotable and make it available at this endpoint.
+     * Get a random cat image from thecatapi and make it available at this endpoint.
      *
-     * @return
+     * @return an html page with the image and formatting
      */
-    @GetMapping("/quote")
-    public Object getQuote() {
-        String url = "https://api.quotable.io/random";
+    @GetMapping("/")
+    public Object getCat() {
+        String url = "https://api.thecatapi.com/v1/images/search";
         RestTemplate restTemplate = new RestTemplate();
-        Object jSonQuote = restTemplate.getForObject(url, Object.class);
 
-        //Print the whole response to console.
-        String quote = restTemplate.getForObject(url, String.class);
-        //Parse out the most important info from the response.
-        JSONObject jo = new JSONObject(quote);
-        System.out.println(jo.toString());
-        String quoteAuthor = jo.getString("author");
-        String quoteContent = jo.getString("content");
-        System.out.println(quoteAuthor);
-        System.out.println(quoteContent);
+        String cat = restTemplate.getForObject(url, String.class);
+        JSONObject jo = new JSONArray(cat).getJSONObject(0);
+        String catUrl = jo.getString("url");
+        int width = jo.getInt("width");
+        int height = jo.getInt("height");
 
-        return jSonQuote;
+        // Normalize the height of the image to 700px
+        width = (int) (width * (500.0 / height));
+        height = 500;
+
+
+        return """
+                <!DOCTYPE html>
+                <html>
+                <head>
+                <title>Random Cat</title>
+                <style>
+                body  {
+                        background-color: #f2f2f2;
+                        align-items: center;
+                        justify-content: center;
+                        display: flex;
+                        flex-direction: column;
+                        }
+                img {
+                        border-radius: 10px;
+                        border: 1px solid #ddd;
+                        padding: 5px;
+                        box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+                        }
+                h1 {
+                        font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+                        color: #AFAA6A;
+                        }
+                </style>
+                </head>
+                <body>
+                <h1>Random Cat</h1>
+                <img src="%s" alt="" width=%s height=%s>
+                                
+                </body>
+                </html>
+                                
+                """.formatted(catUrl, width, height);
     }
 
-    /**
-     * Get a list of universities from hipolabs and parse the most important
-     * info to console.
-     *
-     * @return
-     */
-    @GetMapping("/universities")
-    public List<Object> getUniversities() {
-        String url = "http://universities.hipolabs.com/search?name=sports";
-        RestTemplate restTemplate = new RestTemplate();
-        Object[] universities = restTemplate.getForObject(url, Object[].class);
-
-        //Print the whole response to console
-        JSONArray ja = new JSONArray(universities);
-        System.out.println(ja.toString());
-
-        //Parse out relevant info from each entry in the response
-        Iterator<Object> iter = ja.iterator();
-        while (iter.hasNext()) {
-            JSONObject jo = (JSONObject) iter.next();
-            String name = jo.getString("name");
-            String country = jo.getString("country");
-            System.out.println(name + " : " + country);
-
-        }
-
-        return Arrays.asList(universities);
-    }
 
 }
